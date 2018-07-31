0c64c4050d27263347b01054a05ed08edf1763f2
Ryan Mullowney <ryan@Ryans-MacBook-Pro.local>
2018-07-30 22:37:42 -0500

    Adds portfolio images and puts simple banners over the images in that component

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,8 @@ export default class Home extends Component {
         <h2 className="font-weight-light" >Hey!  I'm Ryan.</h2>
 
         <h3 className="font-weight-light mt-4">I'm a full-stack web developer.</h3>
+
+        <h3 className="font-weight-light mt-4">You can view my resume <a href="">here</a>.</h3>
       </div>
       // <div className="home">
       //   { this.props.children }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,11 +5,15 @@ export default class Portfolio extends Component {
     return (
       <div className="container text-center mt-4">
         <div className="row">
-          <div className="col-6">
+          <div className="col-6 justify-content-center">
             <h2 className="font-weight-light">Politiscape</h2>
+            <img className="" style={{marginBottom: "-90px"}} src="./images/politiscape.gif"/>
+            <div className="d-inline-flex justify-content-center shadow" style={{height: "75px", width: "416px", backgroundColor: "blue" }}><h3 className="font-weight-light text-white mt-3">Politiscape</h3></div>
           </div>
           <div className="col-6">
             <h2 className="font-weight-light">MyCrypto</h2>
+            <img className="" style={{marginBottom: "-90px"}} src="./images/mycrypto.jpg"/>
+            <div className="d-inline-flex justify-content-center shadow" style={{height: "75px", width: "416px", backgroundColor: "blue" }}><h3 className="font-weight-light text-white mt-3">MyCrypto</h3></div>
           </div>
         </div>
       </div>
