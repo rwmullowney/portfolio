97d6bbea7c34d823b7a428472eb3f34f5e1f4e36
Ryan Mullowney <ryan@ryans-mbp.attlocal.net>
2018-08-13 21:10:37 -0500

    Makes icons on contact page larger (although alignment is an ongoing issue).  Shrinks images on Portfolio page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,19 +8,19 @@ export default class Portfolio extends Component {
           <div className="col-6 justify-content-center">
             {/* <h2 className="font-weight-light">Politiscape</h2> */}
             <a href="https://rwmullowney.github.io/politiscape/" target="_blank" style={{ textDecoration: "none" }}>
-              <img className="" style={{ marginBottom: "-90px" }} src="./images/politiscape.gif" />
-              <div className="d-inline-flex justify-content-center shadow" style={{ height: "75px", width: "416px", backgroundColor: "#3F729B" }}><h3 className="font-weight-light text-white mt-3">Politiscape</h3></div>
+              <img className="" style={{ maxHeight: "256px", marginBottom: "-90px" }} src="./images/politiscape.gif" />
+              <div className="d-inline-flex justify-content-center shadow" style={{ height: "75px", width: "285px", backgroundColor: "#3F729B" }}><h3 className="font-weight-light text-white mt-3">Politiscape</h3></div>
             </a>
-            <p className="mt-5 mx-5 font-weight-light">A web app designed to foster political engagement by allowing users to search via zip
+            <p className="mt-4 mx-5 font-weight-light">A web app designed to foster political engagement by allowing users to search via zip
 code to view their representatives, and their contact information</p>
           </div>
           <div className="col-6">
             {/* <h2 className="font-weight-light">MyCrypto</h2> */}
             <a href="http://mycryptocurrency.herokuapp.com/" target="_blank" style={{ textDecoration: "none" }}>
-              <img className="" style={{ marginBottom: "-90px" }} src="./images/mycrypto.jpg" />
-              <div className="d-inline-flex justify-content-center shadow" style={{ height: "75px", width: "416px", backgroundColor: "#3F729B" }}><h3 className="font-weight-light text-white mt-3">MyCrypto</h3></div>
+              <img className="" style={{ maxHeight: "256px", marginBottom: "-90px" }} src="./images/mycrypto.jpg" />
+              <div className="d-inline-flex justify-content-center shadow" style={{ height: "75px", width: "285px", backgroundColor: "#3F729B" }}><h3 className="font-weight-light text-white mt-3">MyCrypto</h3></div>
             </a>
-            <p className="mt-5 mx-5 font-weight-light">A cryptocurrency market simulator that allows you to invest virtual money based on real-time coin values</p>
+            <p className="mt-4 mx-5 font-weight-light">A cryptocurrency market simulator that allows you to invest virtual money based on real-time coin values</p>
           </div>
         </div>
       </div>
