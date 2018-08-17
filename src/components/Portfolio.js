7ee9d67ed73609b7e0d0928a480ee9e0e3d33c75
Ryan Mullowney <ryan@wireless-10-145-166-209.public.utexas.edu>
2018-08-17 14:58:42 -0500

    Adds functioning video tiles for each project on the portfolio page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,26 +6,35 @@ export default class Portfolio extends Component {
     return (
       <div className="container text-center mt-4">
         <div className="row">
+
           <div className="col-4 justify-content-center">
             {/* <h2 className="font-weight-light">Politiscape</h2> */}
             <a href="https://rwmullowney.github.io/politiscape/" target="_blank" style={{ textDecoration: "none" }}>
-              <img className="portfolioImage" src="./images/politiscape.gif" />
-              <div className="d-inline-flex justify-content-center shadow bannerDiv"><h3 className="font-weight-light text-white mt-3">Politiscape</h3></div>
+              {/* <img className="portfolioImage" src="./images/politiscape.gif" /> */}
+              <div className="wrapper">
+                <video className="rounded" src="./images/politiscape.mp4" type="video/mp4" autoPlay loop />
+                <div className="d-inline-flex justify-content-center shadow bannerDiv"><h3 className="font-weight-light text-white mt-3">Politiscape</h3></div>
+              </div>
             </a>
             <p className="mt-4 mx-5 font-weight-light">A web app designed to foster political engagement by allowing users to search via zip
 code to view their representatives, and their contact information</p>
           </div>
+
           <div className="col-4">
             {/* <h2 className="font-weight-light">MyCrypto</h2> */}
             <a href="http://mycryptocurrency.herokuapp.com/" target="_blank" style={{ textDecoration: "none" }}>
-              <img className="portfolioImage" src="./images/mycrypto.jpg" />
-              <div className="d-inline-flex justify-content-center shadow bannerDiv"><h3 className="font-weight-light text-white mt-3">MyCrypto</h3></div>
+              {/* <img className="portfolioImage" src="./images/mycrypto.jpg" /> */}
+              <div className="wrapper">
+                <video className="rounded" src="./images/mycrypto.mp4" type="video/mp4" autoPlay loop />
+                <div className="d-inline-flex justify-content-center shadow bannerDiv"><h3 className="font-weight-light text-white mt-3">MyCrypto</h3></div>
+              </div>
             </a>
             <p className="mt-4 mx-5 font-weight-light">A cryptocurrency market simulator that allows you to invest virtual money based on real-time coin values</p>
           </div>
+
           <div className="col-4">
             <a href="https://rwmullowney.github.io/Hangman-Game/" target="_blank" style={{ textDecoration: "none" }}>
-              {/* <img className="portfolioImage" src="./images/hangman.png" /> */}
+              {/* <img className="portfolioImage rounded" src="./images/hangman.gif" /> */}
               <div className="wrapper">
                 <video className="rounded" src="./images/hangman.mp4" type="video/mp4" autoPlay loop />
                 <div className="d-inline-flex justify-content-center shadow bannerDiv"><h3 className="font-weight-light text-white mt-3">Nintendo Hangman</h3></div>
@@ -33,6 +42,7 @@ code to view their representatives, and their contact information</p>
             </a>
             <p className="mt-4 mx-5 font-weight-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           </div>
+
         </div>
       </div>
       // <div className="projects">
